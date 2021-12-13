9101ef93ff763223548abb72f50ae8d761478f1f
avicohendev <coavr00@gmail.com>
2021-12-13 21:09:49 +0200

    add connection to firebase

---
+++ src/firebase/firebaseApp.ts
@@ -0,0 +1,13 @@
+import { initializeApp, applicationDefault, cert } from'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue} from 'firebase-admin/firestore';
+
+
+
+initializeApp({
+    credential: applicationDefault(),
+   // databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+});
+
+
+export const db = getFirestore();
+
