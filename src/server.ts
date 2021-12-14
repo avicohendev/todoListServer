90128e1b40ce01267e76389a672eefb7446a1d32
avicohendev <coavr00@gmail.com>
2021-12-14 22:10:32 +0200

    example of creating list and adding items

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,43 @@
-import {db} from './firebase/firebaseApp'
+import {db, } from './firebase/firebaseApp'
+import { getFirestore, Timestamp, FieldValue, Transaction} from 'firebase-admin/firestore';
 console.log('aaaa');
 
-const docRef = db.collection('users').doc('alovelace');
 
+const itemStatus = {
+  complete : "complete",
+  notComplete : "notComplete",
+  runningLow: "runningLow"
+}
+const docRef = db.collection('users').doc('alovelace');
 const func = async () => {
-  await docRef.set({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815
+  const listItem1 = await db.collection('listItem').add(
+    {
+      itemName: 'milk',
+      amount: 2,
+      status: itemStatus.runningLow,
+      createDate : FieldValue.serverTimestamp()
+    }
+  )
+  const listItem2 = await db.collection('listItem').add(
+    {
+      itemName: 'bread',
+      amount: 1,
+      status: itemStatus.notComplete,
+      createDate : FieldValue.serverTimestamp()
+    }
+
+    
+    
+  )
+
+  const list = await db.collection('list').add({
+    name: 'victory',
+    user: "ffffff",
+    items: FieldValue.arrayUnion(db.doc(`${listItem1.path}`), db.doc(`${listItem2.path}`)),
+   
   });
+
+  console.log(list);
 }
 
 
